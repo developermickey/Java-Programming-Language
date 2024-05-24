2ce0b0e43b46069fe5ac8d9490766ac0e57f153d
mickeysrishti <Mukeshpathak345@gmail.com>
2024-05-24 13:47:46 +0530

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+// Boilerplate Code 
+
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.println("Hello Technical Mickey");
+        System.out.println("Hello Technical Mickey");
+        System.out.println("Hello Technical Mickey\n");
+    }
+}
